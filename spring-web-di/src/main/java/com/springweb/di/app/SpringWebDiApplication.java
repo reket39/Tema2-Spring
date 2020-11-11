12384b1ddec9d90025c0227542f5e1c334920a3b
Ezequiel Fernandez <ezequiel.info12@gmail.com>
2020-11-11 11:26:28 +0100

    genereic

---
+++ spring-web-di/src/main/java/com/springweb/di/app/SpringWebDiApplication.java
@@ -0,0 +1,13 @@
+package com.springweb.di.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebDiApplication.class, args);
+	}
+
+}
