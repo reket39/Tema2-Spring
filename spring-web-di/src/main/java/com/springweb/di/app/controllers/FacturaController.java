9271b93298d0dc1d176a2fd551edbcbcfcba75e9
Ezequiel Fernandez <ezequiel.info12@gmail.com>
2020-11-12 11:33:00 +0100

    IMPORTANTE ¡¡esta parte repasa:maneja los datos de la factura

--- spring-web-di/src/main/java/com/springweb/di/app/controllers/FacturaController.java
+++ spring-web-di/src/main/java/com/springweb/di/app/controllers/FacturaController.java
@@ -1,5 +1,27 @@
 package com.springweb.di.app.controllers;
 
-public class FacturaController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.springweb.di.app.models.domain.Factura;
+
+@Controller
+@RequestMapping("/factura")
+public class FacturaController {
+    
+	@Autowired
+	private Factura factura;
+	
+	@GetMapping("/ver")
+	public String ver(Model model) {
+		
+		model.addAttribute("factura",factura);
+		model.addAttribute("titulo", "Ejemplo facturas con inyeccion de dependencias");
+		
+		return "factura/ver";
+		
+	}
 }
